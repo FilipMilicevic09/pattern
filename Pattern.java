2cd5b318c38aa226396556fe693d611dbac06b74
FilipMilicevic09 <filip.milicevic009@gmail.com>
2025-10-30 18:28:35 +0000

    x

--- Pattern.java
+++ Pattern.java
@@ -1,1 +1,21 @@
+//name
+// date
+//brief description
 
+public class Pattern{
+   //precondition
+   //postcondition
+
+   public static void stars (int rows){
+    String stars = "*";
+    while (rows > 0) {
+        System.out.println(stars);
+        stars += "**";
+        rows--;
+   }
+}
+    
+    public static void main (String [] args){
+    stars(5);
+    }
+}    
