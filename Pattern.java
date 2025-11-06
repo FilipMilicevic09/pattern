cea8df3d91676542d6ba1d872f09d7fe97141cfc
FilipMilicevic09 <filip.milicevic009@gmail.com>
2025-11-06 18:48:47 +0000

    edfsdz

--- Pattern.java
+++ Pattern.java
@@ -1,12 +1,12 @@
-//name
-// date
+//Filip Milicevic
+// 11/5
 //brief description
 
 public class Pattern{
    //precondition
    //postcondition
 
-   public static void stars (int rows){
+   public static void stars(int rows){
     String stars = "*";
     while (rows > 0) {
         System.out.println(stars);
@@ -14,8 +14,24 @@ public class Pattern{
         rows--;
    }
 }
+   public static void triangle (int rows) {
+    int i = 1;
+    while (i<= rows) {
+        int count = 1;
+        while (count <= i) {
+            System.out.print(i);
+            count++;
+        }
+        System.out.println();
+        i++;
+    }
+
+   }
+   
+
     
     public static void main (String [] args){
-    stars(5);
+    stars(7);
+    triangle(9);
     }
 }    
